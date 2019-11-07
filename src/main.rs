5ea88cad927db232ec27ef88b0c000817920f4f3
willi <willi.kappler@uni-tuebingen.de>
2019-11-07 15:34:12 +0100

    Initial release

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// External crates
+use log::{info, error, debug};
+use log4rs;
+
+fn main() {
+
+    let file_logger = log4rs::append::file::FileAppender::builder()
+        .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new("{d} {l} - {m}{n}")))
+        .build("school_puppy.log").unwrap();
+
+    let log_config = log4rs::config::Config::builder()
+        .appender(log4rs::config::Appender::builder().build("file_logger", Box::new(file_logger)))
+        .build(log4rs::config::Root::builder().appender("file_logger").build(log::LevelFilter::Debug))
+        .unwrap();
+
+    let _log_handle = log4rs::init_config(log_config).unwrap();
+
+}
